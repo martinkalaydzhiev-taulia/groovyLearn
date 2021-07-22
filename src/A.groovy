03f8ed1d1e424bbfa44f5e79bc3672eeb4d4309c
martinkalaydzhiev-taulia <87652109+martinkalaydzhiev-taulia@users.noreply.github.com>
2021-07-22 14:37:49 +0300

    multiple assignment

--- src/A.groovy
+++ src/A.groovy
@@ -1,2 +1,16 @@
 class A {
+    static void main(String[] args) {
+        def s1 = "aaaa"
+        def s2 = "bbbb"
+        (s1, s2) = [s2, s1]
+        println("$s1 and $s2")
+
+        def (a1, b2) = ["ppp", "ooo", "kkk"]
+        assert a1 == "ppp"
+        assert b2 == "ooo"
+        println("$a1 $b2")
+
+        def (a2, a3, a4) = ["kkk", "lll"]
+        assert a4 == null
+    }
 }
